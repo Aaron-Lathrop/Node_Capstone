bd50af3731530fc3c9883abb4354ad436d1ad43f
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-01 20:10:27 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+'use strict';
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
+console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
