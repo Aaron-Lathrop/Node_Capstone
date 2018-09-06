4de3eacfc01264ad9ac79e0dc6244a2534c6964b
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-06 06:58:04 -0600

    refactor index.js

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// $.ajax({
-//     type: "POST",
-//     url: "/",
-//     // data: markers,
-//     contentType: "application/json; charset=utf-8",
-//     dataType: "json",
-//     success: function(data){alert(data);},
-//     failure: function(errMsg) {
-//         alert(errMsg);
-//     }
-// });
+$.ajax({
+    type: "POST",
+    url: "https://stark-thicket-75096.herokuapp.com/",
+    // data: markers,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
