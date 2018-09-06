30f8294a5e80d71f4253c8c61eb3d1beaad4ceb1
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-06 09:21:56 -0600

    refactor server

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ $.ajax({
     type: "POST",
     url: "https://stark-thicket-75096.herokuapp.com",
     // data: JSON.stringify({speech: "message"}),
-    data: data,
+    data: this,
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     success: function(data){return data;},
