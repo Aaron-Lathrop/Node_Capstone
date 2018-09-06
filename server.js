7edfd7b92c04cb05a1ab86e1cd1c0c4eae5273ed
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-05 19:20:03 -0600

    refactor mime type error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "js")));
 
 app.post("/", function(req, res) {
     var speech =
