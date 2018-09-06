9f81925f4bed81d9053bac9e228fd33eac77c334
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-05 20:44:12 -0600

    refactor

--- public/index.js
+++ public/index.js
@@ -1,5 +1,13 @@
 'use strict';
 
-// $.ajax('/',{
-//     success: function(res)
-// });
+$.ajax({
+    type: "POST",
+    url: "/",
+    // data: markers,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
