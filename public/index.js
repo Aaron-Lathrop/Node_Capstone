898236b1093d400497c20141e36ff61a9bca7b06
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-06 06:45:11 -0600

    testing dialogflow response

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-$.ajax({
-    type: "POST",
-    url: "/",
-    // data: markers,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function(data){alert(data);},
-    failure: function(errMsg) {
-        alert(errMsg);
-    }
-});
+// $.ajax({
+//     type: "POST",
+//     url: "/",
+//     // data: markers,
+//     contentType: "application/json; charset=utf-8",
+//     dataType: "json",
+//     success: function(data){alert(data);},
+//     failure: function(errMsg) {
+//         alert(errMsg);
+//     }
+// });
