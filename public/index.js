3046b0a9e6dca6118230e080494567158aba7968
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-06 09:54:54 -0600

    refactor server

--- public/index.js
+++ public/index.js
@@ -1,14 +1,36 @@
 'use strict';
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-$.ajax({
-    type: "POST",
-    url: "https://stark-thicket-75096.herokuapp.com",
-    // data: JSON.stringify({speech: "message"}),
-    data: this,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function(data){return data;},
-    failure: function(errMsg) {
-        alert(errMsg);
-    }
+app.post('/', function(req, res){
+    console.log('Received POST request');
+    if(!req.body) return res.sendStatus(400)
+    res.setHeader('Content-Type', 'application/json');
+    console.log('Here is the post request from DialogFlow');
+    console.log(req.body);
+    console.log(`Echo text is: ${req.body.queryResult.parameters['echoText']}`);
+    let echo = req.body.queryResult.parameters['echoText'];
+    let response = " ";
+    let responseObj = {
+                        "fulfillmentText":response,
+                        "fulfillmentMessages":[{"text": {"text": echo}}]
+                    }
+    console.log(`Here is the resonse to dialogflow: ${responseObj}`);
+    return res.json(responseObj);
 });
+
+// $.ajax({
+//     type: "POST",
+//     url: "https://stark-thicket-75096.herokuapp.com",
+//     // data: JSON.stringify({speech: "message"}),
+//     data: this,
+//     contentType: "application/json; charset=utf-8",
+//     dataType: "json",
+//     success: function(data){return data;},
+//     failure: function(errMsg) {
+//         alert(errMsg);
+//     }
+// });
+
+// https://youtu.be/uZrolwP5NyM?t=415
