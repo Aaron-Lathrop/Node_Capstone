59dae0acf179bb158957374b37f087d88358c7b4
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-11 07:33:36 -0600

    add review screen

--- public/index.js
+++ public/index.js
@@ -71,23 +71,83 @@ const nodeSampleQuestions = {
     ]
 };
 
+const responses = {
+    "responses": [
+        {
+            "id": "1111111",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "2222222",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "3333333",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "4444444",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "5555555",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "6666666",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        {
+            "id": "7777777",
+            "userName": "Aaron",
+            "questionText": "",
+            "userResponse": "",
+            "date": Date
+        },
+        
+    ]
+};
+
 function getPracticeQuestion(questionList){
-    if(questionNumber < questionList.length){
+    
         const question = questionList.questions[questionNumber].questionText;
         questionNumber++;
         return question;
-    }
     
+
+}
+
+function getResponses(){
+
 }
 
 function mockStartHandler() {
     $('#mockStart').click(function(){
+        const question = getPracticeQuestion(nodeSampleQuestions);
         $('#mockInterview').html(`
-        <form id='interview name='interview' autocomplete='off'>
-        <label>${getPracticeQuestion(nodeSampleQuestions)}</label>
-        <textarea rows='10' cols='75' wrap='hard' placeholder='Type your response...' name='Describe the HTTP requests/response lifecycle'></textarea>
-        <input id='answerButton' type='submit' value='Answer'>
-        </form>`);
+            <form id='interview name='interview' autocomplete='off'>
+            <label>${question}</label>
+            <textarea rows='10' cols='75' wrap='hard' placeholder='Type your response...' name='${question}'></textarea>
+            <button id='answerButton' type='submit' value='Answer'>Answer</button>
+            </form>`);
     });
     $(answerButtonHandler());
 }
@@ -95,12 +155,27 @@ function mockStartHandler() {
 function answerButtonHandler() {
     $('#mockInterview').submit("#interview", function(e){
         e.preventDefault();
-        $('#mockInterview').html(`
-        <form id='interview name='interview' autocomplete='off'>
-        <label>${getPracticeQuestion(nodeSampleQuestions)}</label>
-        <textarea rows='10' cols='75' wrap='hard' placeholder='Type your response...' name='Describe the HTTP requests/response lifecycle'></textarea>
-        <input id='answerButton' type='submit' value='Answer'>
-        </form>`);
+        if(questionNumber < nodeSampleQuestions.questions.length){
+            const question = getPracticeQuestion(nodeSampleQuestions);
+            $('#mockInterview').html(`
+                <form id='interview name='interview' autocomplete='off'>
+                <label>${question}</label>
+                <textarea rows='10' cols='75' wrap='hard' placeholder='Type your response...' name='${question}'></textarea>
+                <button id='answerButton' type='submit' value='Answer'>Answer</button>
+                </form>`);
+        } else {
+            $('#mockInterview').html(`
+                <h3>Thank you for your time, this concludes the interview</h3>
+                <p><span class='center'>Click the review button to review your answers.</span></p>
+                <button id='review'>Review Your Answers</button>`);
+            $(reviewButtonHandler());
+        }
+    });
+}
+
+function reviewButtonHandler(){
+    $("#review").click(function(){
+        console.log(`Review button clicked`);
     });
 }
 
