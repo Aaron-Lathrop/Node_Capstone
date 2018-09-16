fe52532bc921850a7ebe8018eaa9417b528d9cf0
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-09-16 16:22:01 -0600

    deploying to heroku"

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/interview-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://mongodb://<dbuser>:<dbpassword>@ds151402.mlab.com:51402/interview-prep';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-interview-app';
 exports.PORT = process.env.PORT || 8080;
